81bdcac202f8f69a273901be1d84f490cc4e8ab3
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-19 13:06:18 +0100

    Create and fill Author class

--- src/model/Author.java
+++ src/model/Author.java
@@ -1,4 +1,27 @@
 package model;
 
-public class Author {
+public class Author extends Person{
+    private Integer authorId;
+    private String surname;
+    private Integer birthYear;
+
+    public Author(Integer authorId, String name, String surname, String city, String country, Integer birthYear) {
+        super(name, city, country);
+        this.authorId = authorId;
+        this.surname = surname;
+        this.birthYear = birthYear;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + surname;
+    }
 }
