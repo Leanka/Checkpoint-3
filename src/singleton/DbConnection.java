84ddd50d585c45c84a21ba8f7350a87b23669b17
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-19 10:01:32 +0100

    Create and fill DbConnection singleton class

--- src/singleton/DbConnection.java
+++ src/singleton/DbConnection.java
@@ -1,12 +1,29 @@
 package singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
-    private static DbConnection ourInstance = new DbConnection();
+    private static Connection connection = setConnection();
 
-    public static DbConnection getInstance() {
-        return ourInstance;
+    public static Connection getConnection() {
+        return connection;
     }
 
-    private DbConnection() {
+    private static Connection setConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:resources/Books.db");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+
     }
+
 }
