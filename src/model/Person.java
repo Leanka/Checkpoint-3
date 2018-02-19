22cdc10c13dde884136f00bada8a94ec701ff3f8
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-19 13:06:48 +0100

    Create and fill Person class

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Person {
+public abstract class Person {
+    protected String name;
+    protected String city;
+    protected String country;
+
+    public Person(String name, String city, String country) {
+        this.name = name;
+        this.city = city;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
