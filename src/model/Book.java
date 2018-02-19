00406855083bede8a955bb13d53562aeeba8304d
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-02-19 13:06:29 +0100

    Create and fill Book class

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,8 +1,38 @@
 package model;
 
 public class Book {
-    private final Integer ISBN;
-    private String author;
+    private Long ISBN;
+
+    private Author author;
     private String title;
-    private String
+    private Publisher publisher;
+    private Integer publicationYear;
+    private Float price;
+    private TypeBook typeBook;
+
+    public Book(Long ISBN, Author author, String title, Publisher publisher, Integer publicationYear, Float price, TypeBook typeBook) {
+        this.ISBN = ISBN;
+        this.author = author;
+        this.title = title;
+        this.publisher = publisher;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.typeBook = typeBook;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Long getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(Long ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String toString(){
+        return ISBN + " \"" + title + "\", " + author.toString();
+    }
 }
